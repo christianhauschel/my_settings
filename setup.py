ca5c5e3fc9378c3ae9a253d81a85d00c4a6ba192
Christian Hauschel <hacs@zhaw.ch>
2022-09-09 15:34:32 +0200

    updates

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 from setuptools import setup
 
-setup(name="my_settings", version="0.2",
-      description="Common settings/functions for plots and other stuff...", packages=["my_settings"], author="Christian Hauschel", zip_safe=False)
+setup(
+      name="my_settings", 
+      version="0.2",
+      description="Common settings/functions for plots and other stuff...", 
+      packages=["my_settings"], 
+      author="Christian Hauschel", 
+      zip_safe=False,
+      install_requires=[
+          "matplotlib>=3.4.3",
+          "numpy>=1.21.2",
+          "seaborn>=0.11.2",
+          "pandas>=1.3.3",
+          "rich>=10.12.0",
+          "proplot>=0.9.3"
+      ]	
+)
+
