fa49a9f831242c0c77e6a93a822342cb8e1c8b1e
Christian Hauschel <hacs@zhaw.ch>
2023-04-03 11:58:35 +0200

    v3 in setup.y

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="my_settings",
-    version="0.2",
+    version="0.3",
     description="Common settings/functions for plots and other stuff...",
     packages=["my_settings"],
     author="Christian Hauschel",
